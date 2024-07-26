c05881d220bcdfbe0bd1b59dd36a003a8e28156a
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-07-26 20:43:13 -0300

    unicafe step 1 to eleven
    [x]1.11*: unicafe, paso 6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,60 +1,64 @@
-const App = () => {
+import { useState } from 'react'
+
+const Button =({handleFeedBack, label})=>{
+  return (
+    <button onClick={handleFeedBack}>{label}</button>
+  )
+}
+const StatisticLine=({value,text})=>{
+  return (
+    <table>
+      <tbody>
+        <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+    </tr>
+    </tbody>
+  </table>
   
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
-const Header=(props)=>{
-  return (<h1>{props.course.name}</h1>)
-}
-
-const Part=(props)=>{
-  return(
-    <p>
-    {props.name} {props.exercises}
-  </p>
-  )
-}
-
-const Content=()=>{
-  return (
-    <div>
-    <Part name={course.parts[0].name} exercises={course.parts[0].exercises} />
-    <Part name={course.parts[1].name} exercises={course.parts[1].exercises} />
-    <Part name={course.parts[2].name} exercises={course.parts[2].exercises} />
-  </div>
-  )
-}
-
-const Total=(props)=>{
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises +props.parts[2].exercises}</p>
     
   )
 }
 
 
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const total=good+bad+neutral;
+  const average=(good-bad)/total 
+const handleFeedBackGood=()=>{
+  setGood(good+1)
+}
+
+const handleFeedBackNeutral=()=>{
+  setNeutral(neutral+1)
+}
+const handleFeedBackBad=()=>{
+  setBad(bad+1)
+}
+
+
 
   return (
     <div>
-     <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      Give  FeedBack
+
+      <div style={{display:'flex', gap:8 , marginTop:20, marginBottom:20}}>
+      <Button label='good' handleFeedBack={handleFeedBackGood}/>
+      <Button label='neutral' handleFeedBack={handleFeedBackNeutral}/>
+      <Button label='bad' handleFeedBack={handleFeedBackBad}/>
+      </div>
+      <h2>Statistics</h2>
+    {total===0? <div>No feedBack given</div>:<>
+      <StatisticLine text='good' value={good}/>
+      <StatisticLine text='bad' value={bad}/>
+      <StatisticLine text='neutral' value={neutral}/>
+      <StatisticLine text='all' value={good+bad+neutral}/>
+      <StatisticLine text='average' value={average}/>
+    </>}
+
+
     </div>
   )
 }
